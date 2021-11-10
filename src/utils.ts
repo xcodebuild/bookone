1c213bf2fb405b9c0a66293372cd9df80f6a2f8f
shejinxin <shejinxin@corp.netease.com>
2021-11-10 16:25:33 +0800

    delete useless file

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,3 @@
-export function sortByIndex(list: string[]) {
-	return list
-		.map((item) => {
-			const [index, name] = item.split('-');
-			return {
-				index: Number.parseInt(index, 10),
-				name,
-				origin: item,
-			};
-		})
-		.sort((a, b) => {
-			return a.index - b.index;
-		})
-		.map((item) => {
-			return item.origin;
-		});
-}
-
 export function readTitleFromMarkdown(content: string) {
 	const REGEX_TITLE = /# (.+)/;
 	return REGEX_TITLE.exec(content)![1];
