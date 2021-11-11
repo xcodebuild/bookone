f913ac37ede928d521d5e4fec8f8e39b3f95bd81
shejinxin <shejinxin@corp.netease.com>
2021-11-11 10:43:43 +0800

    progress

--- src/task.ts
+++ src/task.ts
@@ -1,5 +1,7 @@
 import colors from 'colors';
+import { throttle } from 'lodash';
 import watch from 'node-watch';
+import ora from 'ora';
 
 const tasks: Array<{
 	text: string;
@@ -23,21 +25,38 @@ export function addTask(text: string, run: () => void, listenFile?: string[]) {
             watch(file, {recursive: true}, (evt, name) => {
                 const tip = colors.yellow(`file change ${name}`);
                 addTask(`[${tip}] ${text}`, run);
+                runTasksLazy();
             });
         });
     }
 }
 
-export const runTasks = () => {
+let spinner: any;
+
+
+export const runTasks = async () => {
+
+    spinner = ora('Starting').start();
     const length = tasks.length;
+    spinner.color = 'yellow';
 
     while (tasks.length > 0) {
         const task = tasks.shift();
-        console.log(
-            `[${colors.green(
-                String(length - tasks.length) + ' DONE',
-            )}/${length} TOTAL] ${task?.text}`,
-        );
+        const text = `[${colors.green(
+            String(length - tasks.length) + ' DONE',
+        )}/${length} TOTAL] ${task?.text}`;
+        spinner.text = text;
+        spinner.render();
         task?.run();
     }
+
+    spinner.color = 'green';
+
+    if (!isWatchMode) {
+        spinner.stop();
+    } else {
+        spinner.text = 'Keep watching for change';
+    }
 }
+
+const runTasksLazy = throttle(runTasks, 100);
